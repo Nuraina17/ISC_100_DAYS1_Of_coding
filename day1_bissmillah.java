30ac09b29a9b35e0814c454cc012e2d796b97dac
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-09 17:15:35 +0700

    Update day1_bissmillah.java

--- day1_bissmillah.java
+++ day1_bissmillah.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BIODATA11;
 
-/**
- *
- * @author nuraina
- */
+
 public class day1_bissmillah {
+    public static void main(String[] args) {
+        String Nama = "Nuraina";
+        String Nim   = "D0222052";
+        String Kelas = "Informatika.E";
+        String Alamat = "Wonomulyo";
+        int Angkatan  = 2022;
+        
+        System.out.println("NAMA         :" + Nama);
+        System.out.println("NIM          :" + Nim);
+        System.out.println("KELAS        :" + Kelas);
+        System.out.println("ALAMAT       :" + Alamat);
+        System.out.println("ANGKATAN     :" + Angkatan);
+        
+    }
+    }
+    
+
+
     
 }
