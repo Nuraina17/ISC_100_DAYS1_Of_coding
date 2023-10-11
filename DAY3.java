9228b20b5ce4b59e1a766cdb727b3332f7fea18a
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-11 19:43:37 +0700

    Add files via upload

---
+++ DAY3.java
@@ -0,0 +1,17 @@
+
+package nonprimirif;
+
+
+public class jiwa {
+    public static void main(String[] args) {
+        String inistring ="nuraina";
+        Byte inibyte =19;
+        Short inishort=2004;
+        Integer iniint=55;
+        Float inifloat=765f;
+        Double iniDouble=1.60;
+        Character inicharacter ='B';
+        Boolean iniboolean=true;
+        
+    }
+}
