caa6c13d6c89737c1965066fc9ea1cbcd65a8062
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-14 17:49:01 +0700

    Add files via upload

---
+++ OperasiAritmatika.java
@@ -0,0 +1,42 @@
+
+package operasi;
+
+
+public class OperasiAritmatika {
+    public static void main(String[] args) {
+        int angka1= 20;
+        int angka2 = 5;
+        
+        int hasil;
+        
+        // 1.Penjumlahan
+        
+        hasil = angka1 + angka2; 
+        System.out.println(angka1 + "+" + angka2 + "=" + hasil);
+        
+        // 2. Pengurangan
+        
+        hasil = angka1 - angka2;
+        System.out.println(angka1 +  "-" + angka2 + "=" + hasil);
+        
+        // 3. Perkalian
+        
+        hasil = angka1 * angka2;
+        System.out.println(angka1 +  "*" + angka2 + "=" + hasil);
+        
+        // 4. Pembagian
+        
+        hasil = angka1 / angka2;
+        System.out.println(angka1 +  "/" + angka2 + "=" + hasil);
+        
+        // 5. Modulus
+        
+        hasil = angka1 % angka2;
+        System.out.println(angka1 +  "%" + angka2 + "=" + hasil);
+        
+        
+        
+        
+        
+    }
+}
