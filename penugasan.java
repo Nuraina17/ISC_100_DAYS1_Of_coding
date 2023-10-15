028bd62f7ad5dff8b7451a6ab901e7653007f1f6
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-15 18:12:40 +0700

    Add files via upload

---
+++ penugasan.java
@@ -0,0 +1,42 @@
+package day7;
+
+public class penugasan {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        
+        // pengisian nilai
+        
+        a= 5;
+        b= 10;
+        
+        //penambahan
+        b+= a;
+        System.out.println("Penambahan:" + b);
+        
+        //pengurangan
+        b-= a;
+        System.out.println("Pengurangan:" + b);
+        
+        //perkalian
+        b*= a;
+        System.out.println("Perkalian:" + b);
+        
+        //pembagian
+        b/= a;
+        System.out.println("Pembagian: " + b);
+        
+        b%= a;
+        System.out.println("Modulus: " + b );
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    }
+}
