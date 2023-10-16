369bd162a2eb65ac44ab4138775a76f4c73ddcca
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-16 19:30:49 +0700

    Add files via upload

---
+++ OPERATOR_PERBANDINGAN.java
@@ -0,0 +1,47 @@
+
+package DAY8;
+
+public class OPERATOR_PERBANDINGAN {
+    public static void main(String[] args) {
+        // sama dengan (==)
+        System.out.println("======= sama dengan ======");
+        int a  = 10;
+        int b  = 15;
+        System.out.println("apakah a== b "+(a==b));
+        
+        // tidak sama dengan (!=)
+        System.out.println("======= tidak sama dengan ======");
+         a  = 10;
+         b  = 15;
+        System.out.println("apakah a != b "+(a!=b));
+        
+        // kurang dari (<)
+        
+        System.out.println("======= kurang dari ======");
+        a  = 10;
+        b  = 15;
+        System.out.println("apakah a < b "+(a<b));
+        
+        // lebih dari (>)
+        System.out.println("======= lebih dari ======");
+        a  = 10;
+        b  = 15;
+        System.out.println("apakah a > b "+(a>b));
+        
+        // kurang dari sama dengan (=<)
+        System.out.println("======= kurang dari sama dengan ======");
+        a  = 10;
+        b  = 15;
+        System.out.println("apakah a <= b "+(a<=b));
+        
+        // lebih dari sama dengan (=>)
+        System.out.println("======= lebih dari sama dengan ======");
+        a  = 10;
+        b  = 15;
+        System.out.println("apakah a >= b "+(a>=b));
+        
+        
+            
+        
+    }
+}
