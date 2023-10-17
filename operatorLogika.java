fe01bc98cdc2056e8a4bd479b46dfea312cc7254
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-17 21:44:49 +0700

    Add files via upload

---
+++ operatorLogika.java
@@ -0,0 +1,24 @@
+
+package days_9;
+
+public class operatorLogika {
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+        
+        boolean hasilAnd = a && b;
+        System.out.println("a && b = " + hasilAnd);
+        
+        boolean hasilOr = a || b;
+        System.out.println("a||b = " + hasilOr);
+        
+        boolean hasilNotA = !a;
+        System.out.println("!a = " + hasilNotA);
+        
+        boolean hasilNotB = !b;
+        System.out.println("!b = " + hasilNotB);
+    
+                
+    }
+}
