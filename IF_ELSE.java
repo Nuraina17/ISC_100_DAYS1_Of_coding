729fd58022bf9fd24f174bdc626706e78d25478d
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-25 19:03:53 +0700

    Add files via upload

---
+++ IF_ELSE.java
@@ -0,0 +1,32 @@
+
+package DAYS_16;
+
+import java.util.Scanner;
+
+
+public class IF_ELSE {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jam (00-23): ");
+        int jam = input.nextInt();
+
+        if (jam >= 5 && jam <= 10) {
+            System.out.println("Waktu pagi.");
+        } else if (jam >= 11 && jam <= 13) {
+            System.out.println("Waktu siang.");
+        } else if (jam >= 14 && jam <= 18) {
+            System.out.println("Waktu sore.");
+        } else if ((jam >= 19 && jam <= 23) || (jam >= 0 && jam <= 4)) {
+            System.out.println("Waktu malam.");
+        } else {
+            System.out.println("Jam yang dimasukkan tidak valid.");
+        }
+    }
+
+    }
+ 
+    
+
+
+   
+
