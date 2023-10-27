aebd0ead8e8f24e7c45462d15556ef6241fb9a30
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-10-27 20:02:31 +0700

    Add files via upload

---
+++ Days_19.java
@@ -0,0 +1,23 @@
+
+package Switch_Case;
+
+
+public class Days_19 {
+    public static void main(String[] args) {
+         int Return = 7;
+
+        switch (Return) {
+            case 1:
+                System.out.println("Saya memilih pilihan 1");
+                break;
+            case 2:
+                System.out.println("Saya memilih pilihan 2");
+                break;
+            case 3:
+                System.out.println("Saya memilih pilihan 3");
+                break;
+            default:
+                System.out.println("Pilihan tidak valid");
+        }
+    }
+}
