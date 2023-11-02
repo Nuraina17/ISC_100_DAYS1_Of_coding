dab57654cf1a9cb13c25b3976fa25549bb3d02ee
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-11-02 18:40:08 +0700

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,38 @@
+
+package days25;
+
+public class for_loop {
+    public static void main(String[] args) {
+        
+        System.out.println("saya baru memulai program");
+        
+        System.out.println("iterasi pertama");
+            for (int i = 1; i < 5; i++) {
+                System.out.println("iterasi ke-"+i);
+             
+      
+            }
+        System.out.println("iterasi kedua");
+            for (int i = 0; i <= 9; i++) {
+                System.out.println("iterasi ke-"+i);
+            
+        }
+         System.out.println("iterasi ketiga");
+            for (int i = 10; i >= 7; i--) {
+                System.out.println("iterasi ke-"+i);   
+            
+     
+        }
+         /* System.out.println("iterasi keempat");
+          int i = 2;
+          for(; i < 10 ;){
+                System.out.println("iterasi ke-"+i);
+    }
+          
+        System.out.println("selesai terimah kasih");
+        
+    }*/
+}
+}
+    
+
