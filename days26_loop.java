2dcdb760d9e73161ab3e679d1c3c23e8d1ca421b
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-11-03 20:00:55 +0700

    Add files via upload

---
+++ days26_loop.java
@@ -0,0 +1,26 @@
+
+package days26;
+
+import java.util.Scanner;
+
+
+public class days26_loop {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan tinggi segitiga: ");
+        int tinggi = input.nextInt();
+
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= tinggi - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
