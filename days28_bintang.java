bc6a2f35833794a3e291cf35f0d39e846973ffc2
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-11-05 19:08:46 +0700

    Add files via upload

---
+++ days28_bintang.java
@@ -0,0 +1,16 @@
+
+package days_28;
+
+
+public class days28_bintang {
+    public static void main(String[] args) {
+        int angka = 5;
+        for (int i = 1; i <= angka; i++) {
+            for (int j = 1; j <= angka; j++) {
+                System.out.print("* ");
+                
+            }
+            System.out.println("");
+        }
+    }
+}
