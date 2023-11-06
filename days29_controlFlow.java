9c11b6830fdf7f8e17409fc407d2304ecf931dfc
Nuraina17 <147233014+Nuraina17@users.noreply.github.com>
2023-11-06 19:28:44 +0700

    Add files via upload

---
+++ days29_controlFlow.java
@@ -0,0 +1,38 @@
+
+package days29;
+
+public class days29_controlFlow {
+    //public static void main(String[] args) {
+        
+        /*System.out.println("BREAK");
+        for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+        break; 
+    }
+        System.out.println("Nilai i: " + i);*/
+    
+       /* System.out.println("CONTINUE");
+      
+        for (int i = 1; i <= 5; i++) {
+        if (i == 3) {
+        continue; // Melanjutkan loop tanpa menjalankan iterasi saat i == 3
+        }
+        System.out.println("Nilai i: " + i);
+    }*/
+      public static void main(String[] args) {
+        
+    }
+       public int tambahkan(int a, int b) {
+       int hasil = a + b;
+       return hasil; 
+       
+}
+
+    }
+
+
+
+
+    
+    
+
